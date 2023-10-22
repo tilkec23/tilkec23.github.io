e7704cc888d660026c9461dcde4c3b559fd7693d
tilkec23 <xxtilkec23xx@gmail.com>
2023-10-22 14:47:39 +0200

    updates

--- my-portfolio/next.config.js
+++ my-portfolio/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    basePath: '/tilkec23.github.io',
+    assetPrefix: '/tilkec23.github.io/',
+    output: 'export'
+  };
